0d5600244ca87b8c20e5720fe6434a71f0aa7a51
Justinas Cicėnas <cicenasjustinas@gmail.com>
2018-09-06 16:36:34 +0300

    ADD: Koldunai Meniu

---
+++ public/menus/koldunai.js
@@ -0,0 +1,34 @@
+{
+    "name": "Koldunu namai",
+    "url": "https://www.facebook.com/UAB-Makbela-323052178099222",
+    "imageUrl": "https://junesfoododyssey.files.wordpress.com/2016/02/lithuanian-koldunai-dsc_0073.jpg",
+    "menu": {
+        "categories": [
+            {
+				"name": "Base",
+                "items": [
+                    {
+                        "name": "Koldunai",
+                        "sizes": ["L", "M"],
+                        "items": ["Virti", "Gruzdinti"],
+                        "price": [[5, 5], [3.5, 3.5]]
+                    },
+                    {
+                        "name": "Ceburekas",
+                        "price":  3
+                    }
+                ],
+            }, {
+                "name": "Mesa",
+                "items": [
+                    {
+                        "name": "Kiauliena"
+                    },
+                    {
+                        "name": "Vistiena"
+                    }
+                ]
+            }
+        ]
+    }
+}
